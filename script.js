8e86ce24bf0c1e3c841092059588691485fc5cc7
Aangi-Shah1234 <aangi3shah@gmail.com>
2026-03-30 16:49:20 +0530

    Added new alert logic

---
+++ script.js
@@ -0,0 +1,8 @@
+const del = document.createElement("button");
+del.textContent = "❌";
+del.style.background = "red";
+del.style.color = "white";
+del.style.border = "none";
+del.style.borderRadius = "5px";
+del.style.cursor = "pointer";
+alert("New feature logic working");
